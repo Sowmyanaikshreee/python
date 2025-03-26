2ca0b7b428d4a3bf5875e2254c666ad2e2e8687b
Sowmyashree <Sowmyashree@ad.karanji.com>
2025-03-26 10:00:47 +0530

    updated code

--- Day4/squarepattern_1.py
+++ Day4/squarepattern_1.py
@@ -1,13 +1,26 @@
-rows =5
+# rows =5
 
-for i in range(0,rows):
-    for j in range(0,rows):
+# for i in range(0,rows):
+#     for j in range(0,rows):
 
-        if((i==0) or (j==0) or(i==rows-1) or (j==rows-1)or(j==2)&(i==2) ):
-            print("1",end=" ")
+#         if((i==0) or (j==0) or(i==rows-1) or (j==rows-1)or(j==2)&(i==2) ):
+#             print("1",end=" ")
         
     
-        else:
-            print("0",end=" ")
+#         else:
+#             print("0",end=" ")
         
-    print()
+#     print()
+
+size = int(input("Enter an odd number for the pattern size: "))
+
+if size % 2 == 0:
+    print("Please enter an odd number.")
+else:
+    for i in range(size):
+        for j in range(size):
+            if i == 0 or i == size - 1 or j == 0 or j == size - 1 or (i == size // 2 and j == size // 2):
+                print(1, end=" ")
+            else:
+                print(0, end=" ")
+        print()
